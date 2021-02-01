6d8d7ba261a8909b8d5c44a8f21fc821c6448766
guapirourou <690689726@qq.com>
2021-02-01 12:13:00 +0800

    1

--- spark-realtime/src/main/scala/com/guapirourou/gmall2021/realtime/spark/util/MyEsUtil.scala
+++ spark-realtime/src/main/scala/com/guapirourou/gmall2021/realtime/spark/util/MyEsUtil.scala
@@ -1,5 +1,32 @@
 package com.guapirourou.gmall2021.realtime.spark.util
 
+import io.searchbox.client.config.HttpClientConfig
+import io.searchbox.client.{JestClient, JestClientFactory}
+
+import java.util.Properties
+
 object MyEsUtil {
+    private var factory: JestClientFactory = null
+
+    def build() = {
+        factory = new JestClientFactory
+        val properties: Properties = PropertiesUtil.load("config.properties")
+        val serverUrl: String = properties.getProperty("elasticsearch.server")
+        factory.setHttpClientConfig(new HttpClientConfig.Builder("serverUrl")
+                .multiThreaded(true).maxTotalConnection(20)
+                .connTimeout(10000).readTimeout(10000).build())
+    }
+
+    def getClient: JestClient = {
+        if(factory==null){
+            build()
+        }
+        factory.getObject
+    }
+
+    def main(args: Array[String]): Unit = {
+        val jest: JestClient = getClient
+        jest.execute()
+    }
 
 }
